a286a5ef353612f469805197e80623eb31b2af6f
Clinton-Dais <clintongadavis@gmail.com>
2022-01-19 20:47:29 +0000

    inital commit

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  </div>
+</template>
+
+
+<script lang="ts">
+export default {
+  name: "Home"
+}
+</script>
+
+<script setup lang="ts">
+  import HelloWorld from "../components/HelloWorld.vue"
+// import { ref } from "vue";
+</script>
+
+<style scoped>
+
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: 'jit',
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./src/views/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
